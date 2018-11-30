c967f89a5b0cf6e2edf62a4bba38991286a4b2c5
mandarliev <dimitar.mandarliev@gmail.com>
2018-11-30 20:24:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/fizzbuzz/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android.fizzbuzz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        for (i in 0..30){
+            if (i % 15 == 0) {
+                Log.i("FizzBuzz", i.toString())
+            } else if (i % 5 == 0) {
+                Log.i("Buzz", i.toString())
+            } else if (i % 3 == 0) {
+                Log.i("Fizz", i.toString())
+            }
+        }
+
+    }
+}
